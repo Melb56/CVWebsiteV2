2397e0051bfbc6a87ca39d1af7fa4c76ebc61eca
Mélanie Bruzac <130762906+Melb56@users.noreply.github.com>
2025-11-03 14:36:56 +0100

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -30,9 +30,9 @@ export default function About() {
       <div className="container-about">
         <h1 className='titre'  data-aos ="fade-left" data-aos-duration="1000">À propos</h1>
         <p  data-aos ="fade-left" >
-Passionnée par le web et les interfaces interactives, j’aime donner vie à des idées grâce au code. Ce qui m’anime, c’est la recherche du bon équilibre entre design, fluidité et performance — pour créer des expériences utilisateur à la fois belles et intuitives. <br>
-Après une formation complète en développement web fullstack, j’ai choisi de me spécialiser dans le front-end, où je peux exprimer ma créativité tout en restant proche de la technique. Je travaille avec React, Next.js, HTML, CSS et JavaScript, tout en continuant à explorer de nouvelles approches et technologies. <br>
-Curieuse, autonome et toujours prête à apprendre, j’aime les environnements dynamiques où les idées circulent librement. <br>
+Passionnée par le web et les interfaces interactives, j’aime donner vie à des idées grâce au code. Ce qui m’anime, c’est la recherche du bon équilibre entre design, fluidité et performance — pour créer des expériences utilisateur à la fois belles et intuitives. <br/>
+Après une formation complète en développement web fullstack, j’ai choisi de me spécialiser dans le front-end, où je peux exprimer ma créativité tout en restant proche de la technique. Je travaille avec React, Next.js, HTML, CSS et JavaScript, tout en continuant à explorer de nouvelles approches et technologies. <br/>
+Curieuse, autonome et toujours prête à apprendre, j’aime les environnements dynamiques où les idées circulent librement. <br/>
         </p>
         <div className="btnAbout">
           <a
