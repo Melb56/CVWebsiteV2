c828ec129718550f2d3cac81e6c9e309737b4122
Mélanie Bruzac <130762906+Melb56@users.noreply.github.com>
2025-11-03 14:26:35 +0100

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -30,14 +30,9 @@ export default function About() {
       <div className="container-about">
         <h1 className='titre'  data-aos ="fade-left" data-aos-duration="1000">À propos</h1>
         <p  data-aos ="fade-left" >
-          Titulaire d’un titre professionnel de Développeur Web Full Stack, je suis à la
-          recherche de ma première expérience professionnelle dans le développement web
-          front-end. Je souhaite intégrer une entreprise où je pourrai non seulement mettre à
-          profit mes compétences, mais aussi continuer à en développer de nouvelles. <br />
-          Autonome, créative et dotée d’une grande capacité d’adaptation, je m’épanouis dans
-          des environnements variés et apprécie de relever des défis. Ma motivation et ma
-          curiosité me poussent à constamment apprendre et à contribuer activement aux
-          projets qui me sont confiés.
+Passionnée par le web et les interfaces interactives, j’aime donner vie à des idées grâce au code. Ce qui m’anime, c’est la recherche du bon équilibre entre design, fluidité et performance — pour créer des expériences utilisateur à la fois belles et intuitives. <br>
+Après une formation complète en développement web fullstack, j’ai choisi de me spécialiser dans le front-end, où je peux exprimer ma créativité tout en restant proche de la technique. Je travaille avec React, Next.js, HTML, CSS et JavaScript, tout en continuant à explorer de nouvelles approches et technologies. <br>
+Curieuse, autonome et toujours prête à apprendre, j’aime les environnements dynamiques où les idées circulent librement. <br>
         </p>
         <div className="btnAbout">
           <a
